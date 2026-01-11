7b709fbd44c69af796b4c3a0c0c16f35a7a6ba4e
Samuel1505 <oluwakoredeakinola@gmail.com>
2026-01-11 08:31:35 +0100

    Update configuration with mantle network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,38 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ignition";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    mantle: {
+        url: "https://rpc.mantle.xyz", //mainnet
+        accounts: [process.env.ACCOUNT_PRIVATE_KEY || ""],
+    },
+    mantleSepolia: {
+        url: "https://rpc.sepolia.mantle.xyz", // Sepolia Testnet
+        accounts: [process.env.ACCOUNT_PRIVATE_KEY || ""],
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
 };
 
 export default config;
